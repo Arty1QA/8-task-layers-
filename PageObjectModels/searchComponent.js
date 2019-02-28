2d49f720f34cd9e21ca8e53ab70dda227e723a9a
Arty1QA <45430689+Arty1QA@users.noreply.github.com>
2019-02-28 19:03:59 +0300

    Update searchComponent.js

--- PageObjectModels/searchComponent.js
+++ PageObjectModels/searchComponent.js
@@ -1,30 +1,31 @@
 'use strict';
 
 class Search {
+
     clickSearchIcon() {
-        return element(by.className('fa fa-lg fa-search')).click();
+        return browser.waitForAngular().then(function () {
+            return element(by.className('fa fa-lg fa-search')).click();
+        })
     };
-    
+
     setFileNumber(number) {
-      
-        return browser.wait(ExpectedConditions.visibilityOf(element(by.id('fileNumber'))), 3000, 'error message').then (function () {
-      return element(by.id('fileNumber')).sendKeys(number);
-    })
+        return browser.waitForAngular().then(function () {
+            return element(by.id('fileNumber')).sendKeys(number);
+        })
     };
 
     clickSearchButton() {
-        //return element(by.id('irSearchButton')).click();
-        return browser.wait(ExpectedConditions.visibilityOf(element(by.id('irSearchButton'))), 3000, 'error message').then (function () {
+        return browser.waitForAngular().then(function () {
             return element(by.id('irSearchButton')).click();
-    })
+        })
     };
 
-    verifyFileTreeName () {
-        //return element(by.className('tree-file-name ng-binding')).isDisplayed();
-        return browser.wait(ExpectedConditions.visibilityOf(element(by.className('tree-file-name ng-binding'))), 3000, 'error message').then (function () {
+    verifyFileTreeName() {
+
+        return browser.waitForAngular().then(function () {
             return element(by.className('tree-file-name ng-binding')).isDisplayed();
         })
-};
+    };
 };
 
 module.exports = Search;
